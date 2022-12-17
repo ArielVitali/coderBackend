33b3e4a28b21f574633046e25e5eaa4ea3e13d49
ArielVitali <ariel.gonzalez@salesforce.com>
2022-12-17 12:24:27 -0300

    primer ejercicio

---
+++ clase2/14.js
@@ -0,0 +1,9 @@
+console.log("bllall");
+
+const number = [2, 3, 4];
+
+const cuadrados = number.map((numero) => numero ** 2);
+console.log(cuadrados);
+
+const incluye = cuadrados.includes(16);
+console.log(incluye);
