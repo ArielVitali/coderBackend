05ff7080dc1a01b0390a294790000348d3623e64
ArielVitali <ariel.gonzalez@salesforce.com>
2022-12-18 09:38:34 -0300

    clase 2 + entregable1

---
+++ entregables/clase2/entregableParte1.js
@@ -0,0 +1,67 @@
+class ProductManager {
+  #products;
+
+  constructor() {
+    this.#products = [];
+    this.id = 0;
+  }
+
+  addProduct(
+    title = `Default Prduct ${1 + this.id}`,
+    description = "Pre-setted description",
+    price = 10 + this.id,
+    thumbnail = "main/entregableParte1",
+    code = 99 + this.id,
+    stock = 0
+  ) {
+    try {
+      const codes = this.#products.map((codeItem) => codeItem.code);
+
+      if (!codes.includes(code)) {
+        this.id++;
+
+        const product = {
+          id: this.id,
+          title,
+          description,
+          price,
+          thumbnail,
+          code,
+          stock,
+        };
+
+        this.#products.push(product);
+        return `Product created with id: ${this.id}`;
+      } else {
+        return `The product ${title} has been already added`;
+      }
+    } catch {}
+  }
+
+  getProducts() {
+    return this.#products;
+  }
+
+  getProductById(id) {
+    const product = this.#products.filter((product) => product.id === id);
+    if (product.length == 0) {
+      return `Producto ${id} not found`;
+    } else {
+      console.log(`Product ${id} found`);
+      return product;
+    }
+  }
+}
+
+const prodManager = new ProductManager();
+
+console.log(
+  prodManager.addProduct("vfkmklfd", "vlfevl", 12, "/dlkvmf/", 23234, 1)
+);
+console.log(
+  prodManager.addProduct("vfkmklfd", "vlfevl", 12, "/dlkvmf/", 23234, 1)
+);
+console.log(prodManager.addProduct());
+console.log(prodManager.addProduct());
+console.log(prodManager.getProducts());
+console.log(prodManager.getProductById(4));
